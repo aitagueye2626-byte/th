b589b4273d33717c498dbd7ddf6f5efecb0251a3
aitagueye <aitagueye2626@gmail.com>
2026-06-11 08:15:56 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+
+
+const donneesJSON = {
+
+  "utilisateurs": [
+    {
+      "email": "admin@senthiof.sn",
+      "motdepasse": "1234",
+      "nom": "Aita GUEYE",
+      "role": "admin"
+    },
+    {
+      "email": "client@senthiof.sn",
+      "motdepasse": "1234",
+      "nom": "Coach Aly",
+      "role": "client"
+    }
+  ],
+
+  "transactions": [
+    { "client": "Sall Mbaye",    "date": "10/06/2024", "produit": "Thiof",     "quantite": 50, "prix": 3500 },
+    { "client": "Fall Fatou",    "date": "10/06/2024", "produit": "Yakh",      "quantite": 30, "prix": 2000 },
+    { "client": "Diop Moussa",   "date": "09/06/2024", "produit": "Capitaine", "quantite": 20, "prix": 4500 },
+    { "client": "Ba Aminata",    "date": "09/06/2024", "produit": "Thiof",     "quantite": 15, "prix": 3500 },
+    { "client": "Ndiaye Omar",   "date": "08/06/2024", "produit": "Carpe",     "quantite": 40, "prix": 1800 },
+    { "client": "Diallo Coumba", "date": "08/06/2024", "produit": "Yakh",      "quantite": 25, "prix": 2000 },
+    { "client": "Kane Ibrahima", "date": "07/06/2024", "produit": "Thiof",     "quantite": 60, "prix": 3500 },
+    { "client": "Sow Marième",   "date": "07/06/2024", "produit": "Sole",      "quantite": 10, "prix": 5000 },
+    { "client": "Ly Abdou",      "date": "06/06/2024", "produit": "Capitaine", "quantite": 35, "prix": 4500 },
+    { "client": "Gaye Aissatou", "date": "06/06/2024", "produit": "Carpe",     "quantite": 45, "prix": 1800 }
+  ],
+
+  "produits": [
+    { "nom": "Thiof",     "prix": 3500 },
+    { "nom": "Yakh",      "prix": 2000 },
+    { "nom": "Capitaine", "prix": 4500 },
+    { "nom": "Carpe",     "prix": 1800 },
+    { "nom": "Sole",      "prix": 5000 }
+  ],
+
+  "panier": []
+
+};
